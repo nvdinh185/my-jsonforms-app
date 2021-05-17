86d800f880ae4912e47a5c03cfd001daba80d2a0
nvdinh185 <nvdinh0766777123@gmail.com>
2021-05-17 14:48:03 +0700

    app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { JsonForms } from '@jsonforms/react';
+
+import { person } from '@jsonforms/examples';
+import { materialRenderers, materialCells } from '@jsonforms/material-renderers';
+
+const schema = person.schema;
+const uischema = person.uischema;
+const initialData = person.data;
+
 
 function App() {
+  const [data, setData] = useState(initialData);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <JsonForms
+        schema={schema}
+        uischema={uischema}
+        data={data}
+        renderers={materialRenderers}
+        cells={materialCells}
+        onChange={({ data, _errors }) => setData(data)}
+      />
     </div>
   );
 }
