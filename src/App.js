10899c76865b6f3e9b58e9e123c0c875f0babdae
nvdinh185 <nvdinh0766777123@gmail.com>
2021-05-18 09:36:58 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -2,28 +2,31 @@ import './App.css';
 import React, { useState } from 'react';
 import { JsonForms } from '@jsonforms/react';
 
-import { person } from '@jsonforms/examples';
+// import { person } from '@jsonforms/examples';
 import { materialRenderers, materialCells } from '@jsonforms/material-renderers';
 
-const schema = person.schema;
-const uischema = person.uischema;
-const initialData = person.data;
+import schema from './data/schemajson';
+import uischema from './data/uischemajson';
+import initialData from './data/datajson';
 
+// const schema = person.schema;
+// const uischema = person.uischema;
+// const initialData = person.data;
 
 function App() {
-  const [data, setData] = useState(initialData);
-  return (
-    <div className='App'>
-      <JsonForms
-        schema={schema}
-        uischema={uischema}
-        data={data}
-        renderers={materialRenderers}
-        cells={materialCells}
-        onChange={({ data, _errors }) => setData(data)}
-      />
-    </div>
-  );
+    const [data, setData] = useState(initialData);
+    return (
+        <div className='App'>
+            <JsonForms
+                schema={schema}
+                uischema={uischema}
+                data={data}
+                renderers={materialRenderers}
+                cells={materialCells}
+                onChange={({ data, _errors }) => setData(data)}
+            />
+        </div>
+    );
 }
 
 export default App;
