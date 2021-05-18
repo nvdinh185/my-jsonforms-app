b44f63444352b5e8c4e7daccc9def0c76bc6faf6
nvdinh185 <nvdinh0766777123@gmail.com>
2021-05-18 11:09:44 +0700

    ok2

--- src/App.js
+++ src/App.js
@@ -1,62 +1,17 @@
 import Form from "@rjsf/core";
+import schema2 from './data/schema';
+// import uischema from './data/uischema';
+// import data from './data/formdata';
 
 function App() {
 
     const schema = {
-        title: "A registration form",
-        description: "A simple form example.",
+        title: "Todo",
         type: "object",
-        required: [
-            "firstName",
-            "lastName"
-        ],
+        required: ["title"],
         properties: {
-            firstName: {
-                type: "string",
-                title: "First name",
-                default: "Chuck"
-            },
-            lastName: {
-                type: "string",
-                title: "Last name"
-            },
-            telephone: {
-                type: "string",
-                title: "Telephone",
-                minLength: 10
-            }
-        }
-    }
-
-    const uischema = {
-        firstName: {
-            "ui:autofocus": true,
-            "ui:emptyValue": "",
-            "ui:autocomplete": "family-name"
-        },
-        "lastName": {
-            "ui:emptyValue": "",
-            "ui:autocomplete": "given-name"
-        },
-        "age": {
-            "ui:widget": "updown",
-            "ui:title": "Age of person",
-            "ui:description": "(earthian year)"
-        },
-        "bio": {
-            "ui:widget": "textarea"
-        },
-        "password": {
-            "ui:widget": "password",
-            "ui:help": "Hint: Make it strong!"
-        },
-        "date": {
-            "ui:widget": "alt-datetime"
-        },
-        "telephone": {
-            "ui:options": {
-                "inputType": "tel"
-            }
+            title: { type: "string", title: "Title", default: "A new task" },
+            done: { type: "boolean", title: "Done?", default: false }
         }
     }
 
@@ -65,7 +20,6 @@ function App() {
     return (
         <div className="App">
             <Form schema={schema}
-                // uischema={uischema}
                 onChange={log("changed")}
                 onSubmit={log("submitted")}
                 onError={log("errors")}
---
+++ src/data/formdata.js
@@ -0,0 +1,7 @@
+export default {
+    "firstName": "Chuck123",
+    "lastName": "Norris",
+    "age": 75,
+    "bio": "Roundhouse kicking asses since 1940",
+    "password": "noneed"
+}
