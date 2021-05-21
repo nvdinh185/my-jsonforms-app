fb4fe120e9c6a8614a2d825eb0f288c49b5e135b
nvdinh185 <nvdinh0766777123@gmail.com>
2021-05-21 09:43:37 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -6,36 +6,51 @@ class Clock extends React.Component {
         this.state = { date: new Date() };
     }
 
-    onChange() {
-        this.onchange();
-    }
-
     render() {
         return (
             <div>
-                <h1>Hello, world!</h1>
-                <input type="text" onchange="onChange()" />
+                <h1>Hello, {this.props.name}!</h1>
+                <input type="text" onChange={() => this.props.onChange()} />
+                <button onClick={() => this.props.onClickMe()}>click me!</button>
+                <button onClick={() => this.props.onClickMeToo()}>click me too!</button>
                 <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
             </div>
         );
     }
 };
 
+let test = {
+    date: Date.now()
+}
 export default class App extends React.Component {
     constructor(props) {
         super(props);
-        this.handleChange = this.handleChange.bind(this);
+        this.state = { date: test.date };
     }
-    a = "111";
-    handleChange() {
-        console.log(this.a);
-        this.a = "456";
-        console.log(this.a);
+
+    handleChange(data) {
+        // console.log(data);
+        this.setState({ date: Date.now() });
+        // this.state.date = Date.now();
+        // console.log(test);
+    }
+
+    clickMe = (date) => {
+        console.log(test);
+        console.log(date);
+    }
+
+    clickMeToo = (date) => {
+        console.log(test);
+        test.date = Date.now();
+        console.log(test);
+        console.log(date);
     }
 
     render() {
         return (
-            <Clock onChange={this.handleChange} />
+            <Clock name={this.state.date} onClickMeToo={this.clickMeToo} onClickMe={() => this.clickMe(test.date)}
+                onChange={this.handleChange} />
         )
     }
 };
